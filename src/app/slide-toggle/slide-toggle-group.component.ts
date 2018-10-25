d33815b8be7714570023f1e5a6bd69f8cb4356d5
Erik Tallang <e.tallang@gmail.com>
2018-10-25 21:23:15 +0200

    feat(slide-toggle): fix value change error

--- src/app/slide-toggle/slide-toggle-group.component.ts
+++ src/app/slide-toggle/slide-toggle-group.component.ts
@@ -1,4 +1,4 @@
-import { Component, QueryList, AfterViewInit, Input, ContentChildren, forwardRef } from '@angular/core';
+import { Component, QueryList, AfterViewInit, Input, ContentChildren } from '@angular/core';
 import { ToggleOptionComponent } from './toggle-option/toggle-option.component';
 
 @Component({
@@ -10,17 +10,18 @@ export class SlideToggleGroupComponent implements AfterViewInit {
   @Input() value: string;
   @ContentChildren(ToggleOptionComponent) options: QueryList<ToggleOptionComponent>;
   selectedOption: ToggleOptionComponent;
-  constructor() { }
 
   ngAfterViewInit() {
-    if (this.value && this.options && this.options.length) {
-      this.selectedOption = this.options.find(option => option.value === this.value);
-    } else if (this.options && this.options.length) {
-      this.selectedOption = this.options.first;
-    }
-    this.selectedOption.selected = true;
-
-    this.options.forEach(option => option.group = this);
+    Promise.resolve().then(() => {
+      if (this.value && this.options && this.options.length) {
+        this.selectedOption = this.options.find(option => option.value === this.value);
+      } else if (this.options && this.options.length) {
+        this.selectedOption = this.options.first;
+      }
+      this.selectedOption.selected = true;
+  
+      this.options.forEach(option => option.group = this);
+    })
   }
 
   setSelectedOption(value: string) {
